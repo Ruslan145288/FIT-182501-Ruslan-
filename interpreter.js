3ea264359c9d0e34688bb9039862eba15f30ac54
Felix <257062932+Ruslan145288@users.noreply.github.com>
2026-02-25 07:28:10 +0700

    Add execution methods for variable, assignment and if blocks

--- interpreter.js
+++ interpreter.js
@@ -1,25 +1,59 @@
-class Interpreter {
-    constructor() {
-        this.variables = new Map();
-        this.output = [];
+Interpreter.prototype.executeBlock = function(block, context = {}) {
+    const variables = context.variables || this.variables;
+    
+    switch (block.type) {
+        case BlockTypes.VARIABLE:
+            this.executeVariableDeclaration(block, variables);
+            break;
+        case BlockTypes.ASSIGNMENT:
+            this.executeAssignment(block, variables);
+            break;
+        case BlockTypes.IF:
+            this.executeIf(block, variables);
+            break;
     }
+};
 
-    reset() {
-        this.variables.clear();
-        this.output = [];
-    }
-
-    evaluateExpression(expr, variables) {
-        try {
-            let exprStr = expr.replace(/[a-zA-Z_][a-zA-Z0-9_]*/g, (match) => {
-                if (variables.has(match)) {
-                    return variables.get(match);
-                }
-                throw new Error(`Переменная ${match} не объявлена`);
-            });
-            return Function('"use strict"; return (' + exprStr + ')')();
-        } catch (error) {
-            throw new Error(`Ошибка в выражении: ${error.message}`);
+Interpreter.prototype.executeVariableDeclaration = function(block, variables) {
+    const varNames = block.data.names.split(',').map(name => name.trim());
+    varNames.forEach(name => {
+        if (name) {
+            variables.set(name, 0);
+            this.output.push(`> Объявлена переменная: ${name} = 0`);
         }
+    });
+};
+
+Interpreter.prototype.executeAssignment = function(block, variables) {
+    const varName = block.data.variable;
+    if (!variables.has(varName)) {
+        throw new Error(`Переменная ${varName} не объявлена`);
     }
-}
+    const value = this.evaluateExpression(block.data.expression, variables);
+    variables.set(varName, value);
+    this.output.push(`> ${varName} = ${value}`);
+};
+
+Interpreter.prototype.executeIf = function(block, variables) {
+    const leftValue = this.evaluateExpression(block.data.leftExpr, variables);
+    const rightValue = this.evaluateExpression(block.data.rightExpr, variables);
+    const operator = block.data.operator;
+
+    let conditionResult = false;
+    switch (operator) {
+        case '>': conditionResult = leftValue > rightValue; break;
+        case '<': conditionResult = leftValue < rightValue; break;
+        case '==': conditionResult = leftValue == rightValue; break;
+        case '!=': conditionResult = leftValue != rightValue; break;
+        case '>=': conditionResult = leftValue >= rightValue; break;
+        case '<=': conditionResult = leftValue <= rightValue; break;
+    }
+
+    this.output.push(`> Проверка условия: ${leftValue} ${operator} ${rightValue} = ${conditionResult}`);
+
+    if (conditionResult && block.nestedBlocks) {
+        block.nestedBlocks.forEach(nestedBlock => {
+            this.executeBlock(nestedBlock, { variables });
+        });
+    }
+};
